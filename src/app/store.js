91f6d1100095b5ab05c90f0ac955f193e0272de0
Johan Söderlund <johan.soderlund96@gmail.com>
2024-04-16 17:15:31 +0200

    file structure, store, redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import weatherReducer from "../features/weatherSlice";
+import searchReducer from "../features/searchSlice";
 
 export default configureStore({
   reducer: {
     weather: weatherReducer,
+    search: searchReducer,
   },
 });
