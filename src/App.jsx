48d6cdcf1087d9409baa7fdc96cbbe3e80230e03
Johan Söderlund <johan.soderlund96@gmail.com>
2024-04-23 00:29:40 +0200

    implemented react router, started on nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
-import Header from "../components/Header";
-import Navigation from "../components/Navigation";
-import Content from "../components/Content";
-import Weather from "../components/Weather";
-import Footer from "../components/Footer";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Content from "./components/Content";
+import Weather from "./components/Weather";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <nav>
-        <Navigation />
-      </nav>
-      <main>
-        <Header />
-        <Weather />
-      </main>
-      <article>
-        <Content />
-      </article>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Navigation />
+          <Routes>
+            <Route>
+              <Route path="/" element={<Content />} />
+              <Route path="/site1" element={<Content page="site1" />} />
+              <Route path="/site2" element={<Content page="site2" />} />
+              <Route path="/site3" element={<Content page="site3" />} />
+            </Route>
+          </Routes>
+        </nav>
+        <main>
+          <Header />
+          <Weather />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </Router>
   );
 };
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../styles/Navigation.module.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaSearch } from "react-icons/fa";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <nav>
+      <div className={styles.navContainer}>
+        <div className={styles.navItems}>
+          <ul className={styles.navList}>
+            <li>
+              <NavLink to="/" activeClassName={styles.active}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/site1" activeClassName={styles.active}>
+                Site 1
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/site2" activeClassName={styles.active}>
+                Site 2
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/site3" activeClassName={styles.active}>
+                Site 3
+              </NavLink>
+            </li>
+            <li>
+              <FaSearch />
+            </li>
+            <li>
+              <GiHamburgerMenu />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
