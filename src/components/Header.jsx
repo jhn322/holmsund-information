fc5812979b9b690b876c9c4201514163e8750413
Johan Söderlund <johan.soderlund96@gmail.com>
2024-04-29 02:49:07 +0200

    started on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "../styles/Header.module.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <main className={styles.headerContainer}>
+      <div className={styles.innerHeader}>
+        <div className={styles.header}>
+          Holmsund <br /> information
+          <div className={styles.btnContainer}>
+            <button className={styles.headerBtn}>
+              Klicka här för mer information om detta innehåll!
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Header;
