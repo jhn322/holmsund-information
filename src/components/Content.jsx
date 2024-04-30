eeb0db05d8e078b745275c1c4da0d0c7475bc2e8
Johan Söderlund <johan.soderlund96@gmail.com>
2024-04-30 02:30:09 +0200

    fixed width issue with root, h1, margin, padding and more styling adjustment

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,7 +7,7 @@ const Content = () => {
       <div className={styles.innerContent}>
         <div className={styles.content}>
           <article className={styles.textContent}>
-            <h2>Holmsund, Umeå</h2>
+            <h2 className={styles.titleContent}>Holmsund, Umeå</h2>
             <h3 className={styles.undertitleContent}>
               DET HÄR ÄR EN LÅNG UNDERRUBRIK. COOLT.
             </h3>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
     <main className={styles.headerContainer}>
       <div className={styles.innerHeader}>
         <div className={styles.header}>
-          Holmsund <br /> Information
+          <h1 className={styles.headerTitle}>
+            Holmsund <br /> Information
+          </h1>
           <div className={styles.btnContainer}>
             <button className={styles.headerBtn}>
               Klicka här för mer information om detta innehåll!
