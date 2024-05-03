ff71f161e42bc106c044e9d1891a4919d5cb70d1
Johan Söderlund <johan.soderlund96@gmail.com>
2024-05-03 14:30:38 +0200

    worked on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "../styles/Footer.module.css";
+
+// Icons
+import { FaGithub, FaGoogle, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className={styles.footerContainer}>
+      <div className={styles.footerInner}>
+        <div className={styles.footerIcons}>
+          <FaGithub className={styles.github} />
+          <FaGoogle className={styles.google} />
+          <FaInstagram className={styles.instagram} />
+        </div>
+        <div className={styles.footerText}>
+          <ul>
+            <li>Hem</li>
+            <li>Om oss</li>
+            <li>Användarvillkor</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
