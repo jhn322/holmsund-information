29e113d1ff3b67829dc3264f01c0baf1c974367f
Johan Söderlund <johan.soderlund96@gmail.com>
2024-06-11 03:18:31 +0200

    share section is now responsive

--- src/components/ShareExtra.jsx
+++ src/components/ShareExtra.jsx
@@ -24,58 +24,61 @@ const ShareExtra = () => {
               style={{ backgroundImage: `url(${shareImage})` }}
             ></aside>
           </div>
-          {/* Navigate to weather section */}
-          <div className={styles.weatherContainer}>
-            <div className={styles.weatherInner}>
-              <div className={styles.weatherText}>
-                <div className={styles.weather}>
-                  <h3>Väder Information</h3>
-                </div>
-                <div className={styles.weatherContent}>
+          <div className={styles.extraContainer}>
+            {/* Navigate to weather section */}
+            <div className={styles.weatherContainer}>
+              <div className={styles.weatherInner}>
+                <div className={styles.weatherText}>
                   <div className={styles.weather}>
-                    <p>
-                      Här kan du hitta väderprognoser för Homsund, inklusive
-                      temperatur, nederbörd och vindförhållanden för de kommande
-                      dagarna. Uppdateringar sker regelbundet för att ge dig den
-                      mest aktuella väderinformationen.
-                    </p>
+                    <h3>Väder Information</h3>
+                  </div>
+                  <div className={styles.weatherContent}>
+                    <div className={styles.weather}>
+                      <p>
+                        Här kan du hitta regelbundet uppdaterade väderprognoser
+                        för Homsund, inklusive temperatur, nederbörd och
+                        vindförhållanden för de kommande dagarna.
+                      </p>
+                    </div>
                   </div>
                 </div>
+                <NavLink to="/väder">
+                  {" "}
+                  <p className={styles.weatherBtnText}>
+                    Kolla vädret i Holmsund
+                  </p>{" "}
+                </NavLink>
               </div>
-              <NavLink to="/väder">
-                {" "}
-                <p className={styles.weatherBtnText}>
-                  Kolla vädret i Holmsund
-                </p>{" "}
-              </NavLink>
             </div>
-          </div>
-          {/* Navigate to maps section */}
-          <div className={styles.mapContainer}>
-            <div className={styles.mapInner}>
-              <div className={styles.mapText}>
-                <div className={styles.map}>
-                  <h3>Platsinformation</h3>
+            {/* Navigate to maps section */}
+            <div className={styles.mapContainer}>
+              <div className={styles.mapInner}>
+                <div className={styles.mapText}>
+                  <div className={styles.map}>
+                    <h3>Platsinformation</h3>
+                  </div>
+                  <div className={styles.mapContent}>
+                    <div className={styles.map}>
+                      <p>Kulgränd 1 Holmsund</p>
+                    </div>
+                    <div className={styles.map}>
+                      <p>Umeå Västerbotten</p>
+                    </div>
+                    <div className={styles.map}>
+                      <p>Sverige 913 32</p>
+                    </div>
+                    <div className={styles.map}>
+                      <p>63.7066° N / 20.3686° E</p>
+                    </div>
+                  </div>
                 </div>
-                <div className={styles.mapContent}>
-                  <div className={styles.map}>
-                    <p>Kulgränd 1 Holmsund</p>
-                  </div>
-                  <div className={styles.map}>
-                    <p>Umeå Västerbotten</p>
-                  </div>
-                  <div className={styles.map}>
-                    <p>Sverige 913 32</p>
-                  </div>
-                  <div className={styles.map}>
-                    <p>63.7066° N / 20.3686° E</p>
-                  </div>
-                </div>
+                <NavLink to="/karta">
+                  {" "}
+                  <p className={styles.mapBtnText}>
+                    Se karta över Holmsund
+                  </p>{" "}
+                </NavLink>
               </div>
-              <NavLink to="/karta">
-                {" "}
-                <p className={styles.mapBtnText}>Se karta över Holmsund</p>{" "}
-              </NavLink>
             </div>
           </div>
         </div>
