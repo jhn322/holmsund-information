4ed4036ed0a7bca012083fa4baa1bc9e014ce412
Johan Söderlund <johan.soderlund96@gmail.com>
2024-06-20 20:56:56 +0200

    attempting to reduce flicker effect in img change

--- src/components/addon/HeaderAddon.jsx
+++ src/components/addon/HeaderAddon.jsx
@@ -1,18 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // CSS
 import styles from "../../styles/addon/HeaderAddon.module.css";
 
 const HeaderAddon = ({ title, backgroundImage }) => {
+  const [loadedImage, setLoadedImage] = useState(backgroundImage);
+  const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
     const img = new Image();
     img.src = backgroundImage;
+    img.onload = () => {
+      setLoadedImage(backgroundImage);
+      setIsLoaded(true);
+    };
   }, [backgroundImage]);
 
   return (
     <header
-      className={styles.headerContainer}
-      style={{ backgroundImage: `url(${backgroundImage})` }}
+      className={`${styles.headerContainer} ${isLoaded ? styles.loaded : ""}`}
+      style={{ backgroundImage: `url(${loadedImage})` }}
     >
       <h1 className={styles.headerTitle}>{title}</h1>
     </header>
