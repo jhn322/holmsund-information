f1f1b6fcb30afde12ac07bb7a41c1faa3c5793d7
Johan Söderlund <johan.soderlund96@gmail.com>
2024-06-27 17:16:56 +0200

    first styling for map page

--- src/components/pages/MapPage.jsx
+++ src/components/pages/MapPage.jsx
@@ -43,7 +43,22 @@ const MapPage = () => {
       galleryTitle2="Upptäck 1"
     >
       <div className={styles.container}>
-        <h2>UNDER CONSTRUCTION</h2>
+        <h2 className={styles.title}>Holmsund</h2>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+          quibusdam reiciendis est possimus alias rem rerum nihil ipsa sequi?
+          Quae corrupti blanditiis a odio asperiores aliquam dicta consequatur
+          magni numquam?
+        </p>
+        <div className={styles.googleMaps}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43502.27002035805!2d20.358895128173327!3d63.70981450424567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x467c51664f312b5f%3A0x4034506de8c8530!2sHolmsund%2C%20Sweden!5e0!3m2!1sen!2sse!4v1697549523948!5m2!1sen!2sse"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Map of Holmsund"
+          ></iframe>
+        </div>
       </div>
     </LayoutMainPage>
   );
