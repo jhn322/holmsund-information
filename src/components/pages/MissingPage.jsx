b8eaafa5c6624fe3464fa56e7a53d495ec3db51e
Johan Söderlund <johan.soderlund96@gmail.com>
2024-07-09 01:13:21 +0200

    fixed broken img in missing page

--- src/components/pages/MissingPage.jsx
+++ src/components/pages/MissingPage.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../../styles/pages/MissingPage.module.css";
+import NotFoundImage from "../../assets/other/404.png";
 
 const MissingPage = () => {
   return (
     <main className={styles.MissingContainer}>
+      <img
+        src={NotFoundImage}
+        alt="404 Not Found"
+        className={styles.MissingImage}
+      />
       <p className={styles.MissingText}>
         Oops. Vi kunde inte hitta sidan du letade efter :(
       </p>
-      {/* Go Home */}
       <button className={styles.MissingBtn}>
         <Link to="/" className={styles.LinkHome}>
           Gå tillbaka hem
