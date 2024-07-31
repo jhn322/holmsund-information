d392c740042967a898ec6d3c25e148aa16ac90a9
Johan Söderlund <johan.soderlund96@gmail.com>
2024-07-31 20:49:17 +0200

    gtag fixes

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,18 @@ import "./index.css";
 const trackingId = import.meta.env.VITE_GA_TRACKING_ID;
 
 if (trackingId) {
-  // Initialize Google Analytics
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    window.dataLayer.push(arguments);
-  }
-  gtag("js", new Date());
-  gtag("config", trackingId, { debug_mode: true });
-
   const script = document.createElement("script");
   script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
   script.async = true;
+  script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", trackingId, { debug_mode: true });
+    window.gtag = gtag;
+  };
   document.head.appendChild(script);
 }
 
